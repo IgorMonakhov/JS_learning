924420b910eea33d9ad3a935d154260568de4e2d
IgorMonakhov <igor-monakhov@mail.ru>
2022-04-19 23:24:06 +0300

    Исправлен запуск ссылки

--- yandex_bot.js
+++ yandex_bot.js
@@ -10,13 +10,12 @@
 // ==/UserScript==
 
 
-let keywords = ["10 самых популярных шрифтов от Google", "Отключение редакций и ревизий в WordPress", "Вывод произвольных типов записей и полей в WordPress"];
+let keywords = ["купля-продажа авто", "каталог автомобилей", "автомобили купить", "б\у авто"];
 let num = keywords.length;
 let randind = getRandom(0, num);
 let keyword = keywords[randind];
 let btnK = document.getElementsByClassName("button mini-suggest__button")[0];
 let links = document.links;
-
 
 if(btnK !== undefined) {
   //let qElementArr = document.getElementsByName("q");
@@ -24,20 +23,21 @@ if(btnK !== undefined) {
   //qElement.value = keyword;
   console.log(btnK);
   document.getElementsByName("text")[0].value = keyword;
-  setTimeout(clickBtn, 2000);
+ setTimeout(clickBtn, 2000);
 } else {
   for (let i = 0; i < links.length; i++) {
-    if (links[i].href.indexOf("napli.ru") !== -1) {
+    if (links[i].href.indexOf("auto.ru") !== -1) {
       let link = links[i]
-      console.log("Нашел строку " + links[i]);
-      link.click();
+     console.log("Нашел строку " + links[i]);
+ window.location.replace(link);
+//link.click();
       break;
     }
   }
 }
 
 function getRandom(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
+return Math.floor(Math.random() * (max - min) + min);
 }
 function clickBtn(){
   document.getElementsByClassName("button mini-suggest__button")[0].click();
